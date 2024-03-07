361f17b0829337a8debf7dcfe945b42f36a265b1
Cheikyadam <urbaingande@gmail.com>
2024-03-07 15:04:12 -0800

    0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#preparing my servers
+
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+
+dir="/data/"
+if [ ! -d "$dir" ]; then
+        sudo mkdir $dir
+fi
+
+dir="/data/web_static/"
+if [ ! -d "$dir" ]; then
+        sudo mkdir $dir
+fi
+
+dir="/data/web_static/releases/"
+if [ ! -d "$dir" ]; then
+        sudo mkdir $dir
+fi
+
+dir="/data/web_static/shared/"
+if [ ! -d "$dir" ]; then
+        sudo mkdir $dir
+fi
+
+dir="/data/web_static/releases/test/"
+if [ ! -d "$dir" ]; then
+        sudo mkdir $dir
+fi
+
+content="<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>"
+sudo echo -e "$content" > /data/web_static/releases/test/index.html
+
+source_path="/data/web_static/releases/test/"
+dest_path="/data/web_static/current"
+if [ -L "$dest_path" ]; then
+        sudo rm "$dest_path"
+fi
+sudo ln -s "$source_path" "$dest_path"
+
+sudo chown -R ubuntu:ubuntu /data/
+
+new="server_name _;\n\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}"
+sudo sed -i "s/server_name _\;/$new/" /etc/nginx/sites-available/default
+
+sudo nginx -s reload
