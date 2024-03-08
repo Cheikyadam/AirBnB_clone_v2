d94eb42df44d4e8e097af138695033b1ee9ab1a7
Cheikyadam <urbaingande@gmail.com>
2024-03-08 08:48:27 -0800

    2 upd

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,18 +4,18 @@ from os.path import exists, isdir
 from fabric.api import run, put, env, local
 from datetime import datetime
 env.hosts = ['3.90.70.250', '52.87.230.58']
-
+env.user = "ubuntu"
 
 def do_pack():
     """the function"""
     try:
-        if exists("versions") is False or isdir("versions") is False:
+        if isdir("versions") is False:
             local("mkdir versions")
         current_date = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_name = f"versions/web_static_{current_date}.tgz"
+        archive_name = "versions/web_static_{}.tgz".format(current_date)
         local("tar -cvzf {} web_static".format(archive_name))
         return archive_name
-    except Exception:
+    except:
         return None
 
 
@@ -28,13 +28,13 @@ def do_deploy(archive_path):
         tmp_path = archive_path.split('/')[-1]
         path_rel = "/data/web_static/releases/"
         del_ext = tmp_path.split('.')[0]
-        run(f'mkdir -p {path_rel}{del_ext}/')
+        run('mkdir -p {}{del_ext}/'.format(path_rel))
         run('tar -xzf /tmp/{} -C {}{}/'.format(tmp_path, path_rel, del_ext))
-        run(f'rm /tmp/{tmp_path}')
+        run('rm /tmp/{}'.format(tmp_path))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path_rel, del_ext))
         run('rm -rf {}{}/web_static'.format(path_rel, del_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path_rel, del_ext))
         return True
-    except Exception:
+    except:
         return False
