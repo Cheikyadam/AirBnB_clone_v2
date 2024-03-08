0da2fdf0c5ff803f2f939d11bb98792182b3610f
Cheikyadam <urbaingande@gmail.com>
2024-03-08 09:25:29 -0800

    2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ from fabric.api import run, put, env, local
 from datetime import datetime
 env.hosts = ['3.90.70.250', '52.87.230.58']
 env.user = "ubuntu"
-env.key_filename = "~/.ssh/my_ssh_private_key"
+env.key_filename = "~/.ssh/school"
 
 def do_pack():
     """the function"""
@@ -16,7 +16,7 @@ def do_pack():
         archive_name = "versions/web_static_{}.tgz".format(current_date)
         local("tar -cvzf {} web_static".format(archive_name))
         return archive_name
-    except:
+    except Exception:
         return None
 
 
@@ -25,17 +25,17 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-        put(archive_path, '/tmp/')
+        sudo(put(archive_path, '/tmp/'))
         tmp_path = archive_path.split('/')[-1]
         path_rel = "/data/web_static/releases/"
         del_ext = tmp_path.split('.')[0]
-        run('mkdir -p {}{del_ext}/'.format(path_rel))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(tmp_path, path_rel, del_ext))
-        run('rm /tmp/{}'.format(tmp_path))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path_rel, del_ext))
-        run('rm -rf {}{}/web_static'.format(path_rel, del_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path_rel, del_ext))
+        sudo(run('mkdir -p {}{del_ext}/'.format(path_rel)))
+        sudo(run('tar -xzf /tmp/{} -C {}{}/'.format(tmp_path, path_rel, del_ext)))
+        sudo(run('rm /tmp/{}'.format(tmp_path)))
+        sudo(run('mv {0}{1}/web_static/* {0}{1}/'.format(path_rel, del_ext)))
+        sudo(run('rm -rf {}{}/web_static'.format(path_rel, del_ext)))
+        sudo(run('rm -rf /data/web_static/current'))
+        sudo(run('ln -s {}{}/ /data/web_static/current'.format(path_rel, del_ext)))
         return True
-    except:
+    except Exception:
         return False
