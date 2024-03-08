dc2692f69129313bbf5508f44fa0878103cf9916
Cheikyadam <urbaingande@gmail.com>
2024-03-08 12:04:12 -0800

    pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Putting in servers"""
 from os.path import exists, isdir
-from fabric.api import run, put, env, local
+from fabric.api import run, put, env, local, sudo
 from datetime import datetime
 env.hosts = ['3.90.70.250', '52.87.230.58']
 env.user = "ubuntu"
 env.key_filename = "~/.ssh/school"
+
 
 def do_pack():
     """the function"""
@@ -29,13 +30,20 @@ def do_deploy(archive_path):
         tmp_path = archive_path.split('/')[-1]
         path_rel = "/data/web_static/releases/"
         del_ext = tmp_path.split('.')[0]
-        sudo(run('mkdir -p {}{del_ext}/'.format(path_rel)))
-        sudo(run('tar -xzf /tmp/{} -C {}{}/'.format(tmp_path, path_rel, del_ext)))
-        sudo(run('rm /tmp/{}'.format(tmp_path)))
-        sudo(run('mv {0}{1}/web_static/* {0}{1}/'.format(path_rel, del_ext)))
-        sudo(run('rm -rf {}{}/web_static'.format(path_rel, del_ext)))
-        sudo(run('rm -rf /data/web_static/current'))
-        sudo(run('ln -s {}{}/ /data/web_static/current'.format(path_rel, del_ext)))
+        run(sudo('mkdir -p {}{del_ext}/'.format(path_rel)))
+        sudo(
+                run(
+                    'tar -xzf /tmp/{} -C {}{}/'
+                    .format(tmp_path, path_rel, del_ext)))
+        run(sudo('rm /tmp/{}'.format(tmp_path)))
+        run(sudo('mv {0}{1}/web_static/* {0}{1}/'.format(path_rel, del_ext)))
+        run(sudo('rm -rf {}{}/web_static'.format(path_rel, del_ext)))
+        run(sudo('rm -rf /data/web_static/current'))
+        run(
+                sudo(
+                    'ln -s {}{}/ /data/web_static/current'
+                    .format(path_rel, del_ext)))
         return True
-    except Exception:
+    except Exception as e:
+        print(e)
         return False
