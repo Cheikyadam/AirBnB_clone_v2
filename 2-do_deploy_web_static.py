fbb68f81383fd90e0aa8f0fac70ebe3edb24b651
Cheikyadam <urbaingande@gmail.com>
2024-03-07 17:31:17 -0800

    2,3

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Putting in servers"""
+from os.path import exists
+from fabric.api import run, put
+
+
+def do_deploy(archive_path):
+    """Deploiemnt"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, '/tmp/')
+        tmp_path = archive_path.split('/')[-1]
+        path_rel = "/data/web_static/releases/"
+        del_ext = tmp_path.split('.')[0]
+        run(f'mkdir -p {path_rel}{del_ext}/')
+        run('tar -xzf /tmp/{} -C {}{}/'.format(tmp_path, path_rel, del_ext))
+        run(f'rm /tmp/{tmp_path}')
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path_rel, del_ext))
+        run('rm -rf {}{}/web_static'.format(path_rel, del_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path_rel, del_ext))
+        return True
+    except Exception:
+        return False
