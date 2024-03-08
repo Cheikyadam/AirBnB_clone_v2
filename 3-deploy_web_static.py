fad688ae7a97c1b43ca274d646a4ccf69b212588
Cheikyadam <urbaingande@gmail.com>
2024-03-07 18:07:18 -0800

    3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,11 +1,47 @@
 #!/usr/bin/python3
-"""Full deploy"""
-do_pack = __import__("1-pack_web_static").do_pack
-do_deploy = __import__("2-do_deploy_web_static").do_deploy
+"""Putting in servers"""
+from os.path import exists, isdir
+from fabric.api import run, put, env, local
+from datetime import datetime
+env.hosts = ['3.90.70.250', '52.87.230.58']
+
+
+def do_pack():
+    """the function"""
+    try:
+        if exists("versions") is False or isdir("versions") is False:
+            local("mkdir versions")
+        current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_name = f"versions/web_static_{current_date}.tgz"
+        local("tar -cvzf {} web_static".format(archive_name))
+        return archive_name
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """Deploiemnt"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, '/tmp/')
+        tmp_path = archive_path.split('/')[-1]
+        path_rel = "/data/web_static/releases/"
+        del_ext = tmp_path.split('.')[0]
+        run(f'mkdir -p {path_rel}{del_ext}/')
+        run('tar -xzf /tmp/{} -C {}{}/'.format(tmp_path, path_rel, del_ext))
+        run(f'rm /tmp/{tmp_path}')
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path_rel, del_ext))
+        run('rm -rf {}{}/web_static'.format(path_rel, del_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path_rel, del_ext))
+        return True
+    except Exception:
+        return False
 
 
 def deploy():
-    """do_pack() and do_deploy() together"""
+    """create archive and send it to the servers"""
     archive_path = do_pack()
     if archive_path is None:
         return False
