3a686490c0d84787bd7e52721f20453541b41866
Cheikyadam <urbaingande@gmail.com>
2024-03-27 18:56:01 -0700

    8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,6 +5,7 @@ from markupsafe import escape
 from flask import render_template
 from models import storage
 from models.state import State
+from models.city import City
 from os import getenv
 
 
@@ -17,12 +18,13 @@ def teardown_app_context(exception=None):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def index():
-    """hello hbnb"""
+    """cities by state"""
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        states = storage.all(State)
-        return render_template('7-states_list.html', states=states)
+        #states = storage.all(State)
+        
+        return render_template('8-cities_by_states.html', states=storage)
 
 
 if __name__ == '__main__':
